f965e414417297aef8f1ec26c2b896e185f112c2
roseval <roseval@algartelecom.com.br>
2020-05-01 17:35:03 -0300

    Configuration

--- main.tf
+++ main.tf
@@ -1,2 +1,10 @@
 provider "openstack" {
 }
+
+resource "openstack_compute_instance_v2" "instances" {
+  name        = "vm-teste"
+  count       = "1"
+  flavor_name = "small-1"
+  image_name  = "Centos-7"
+  key_pair    = "openstack-teste"
+}
