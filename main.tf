668f848e5bbbc2c8943f468aa087e358898b96ec
roseval <roseval@algartelecom.com.br>
2020-05-08 01:39:04 -0300

    config

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
 provider "openstack" {
+  user_name   = "spinnaker"
+  tenant_name = "tenant0021"
+  password    = "gogogo2020"
+  auth_url    = "http://openstack.privatecloud:5000/v2.0"
+}
+
+terraform {
+  backend "swift" {
+    container = "tf-swift-vm-teste"
+  }
 }
 
 resource "openstack_compute_instance_v2" "instances" {
