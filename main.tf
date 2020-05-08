fd1f6d26dbe909e24558c8068c8b55b7ca88f6c8
roseval <roseval@algartelecom.com.br>
2020-05-08 16:30:35 -0300

    teste

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 provider "openstack" {
   user_name   = "spinnaker"
   tenant_name = "tenant0021"
-  tenant_id   = "83733e5dc93240d4884fce417a83a28a"
   password    = "gogogo2020"
   auth_url    = "http://openstack.privatecloud:5000/v2.0"
-  region      = "RegionOne"
 }
 
 //terraform {
@@ -21,6 +19,7 @@ resource "openstack_compute_instance_v2" "instances" {
   name        = "vm-teste"
   count       = "1"
   flavor_name = "small-1"
-  image_name  = "Centos-7"
-  key_pair    = "openstack-teste"
+  image_name  = "CentOS 7.5.1804"
+  key_pair    = "spinnaker-keypair"
+  network     = { name="privatenetwork-tenant0021"}
 }
